3dba6d8aac56397b139dc71a516a1956a405f5f6
Jack Cable <jackhcable@gmail.com>
2023-02-08 14:03:11 -0500

    Add quotes and bugfix

--- recover.sh
+++ recover.sh
@@ -1,8 +1,8 @@
 mkdir encrypted_files
 
 echo -e "Moving encrypted $1.vmdk to encrypted_files"
-mv $1.vmdk encrypted_files/$1.vmdk
-file_size=$(ls -la $1-flat.vmdk | awk '{print $5}')
+mv "$1.vmdk" "encrypted_files/$1.vmdk"
+file_size=$(ls -la "$1-flat.vmdk" | awk '{print $5}')
 
 echo -e "\nCreating copy of $1-flat.vmdk"
 vmkfstools -c $file_size -d thin temp.vmdk
@@ -10,16 +10,14 @@ rm temp-flat.vmdk
 
 echo -e "\nAdding $1.vmdk"
 sed -i "s/temp-flat/${1}-flat/" temp.vmdk
-if [ "$#" -ne 1 ]; then
-  if [ $2 != "thin" ]; then
-    sed -i '/ddb.thinProvisioned/d' temp.vmdk
-  fi
+if [ "$#" = 1 ] || [ $2 != "thin" ]; then
+  sed -i '/ddb.thinProvisioned/d' temp.vmdk
 fi
-mv temp.vmdk $1.vmdk
+mv temp.vmdk "$1.vmdk"
 
 echo -e "\nCopying $1.vmx"
-mv $1.vmx encrypted_files/$1.vmx
-cp $1.vmx~ $1.vmx
+mv "$1.vmx" "encrypted_files/$1.vmx"
+cp "$1.vmx~" "$1.vmx"
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -27,23 +25,23 @@ if [ $retVal -ne 0 ]; then
 fi
 
 echo -e "\nMoving encrypted $1.vmsd to encrypted_files"
-mv $1.vmsd encrypted_files/$1.vmsd
+mv "$1.vmsd" "encrypted_files/$1.vmsd"
 
 echo -e "\nMoving encrypted $1.nvram to encrypted_files"
-mv $1.nvram encrypted_files/$1.nvram
+mv "$1.nvram" "encrypted_files/$1.nvram"
 
 echo -e ""
 
 echo -e "\nValidating..."
-vmkfstools -e $1.vmdk
+vmkfstools -e "$1.vmdk"
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo -e "\nError. Trying to update the file size."
     file_size_num=$(( file_size / 512 ))
     file_size_num_plus_one=$(( file_size_num + 1 ))
-    sed -i "s/${file_size_num_plus_one}/${file_size_num}/" $1.vmdk
-    vmkfstools -e $1.vmdk
+    sed -i "s/${file_size_num_plus_one}/${file_size_num}/" "$1.vmdk"
+    vmkfstools -e "$1.vmdk"
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo -e "\nError. Could not recover."
